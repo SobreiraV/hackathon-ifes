7513485afc49caac82f03a6fc519e67dcbc956d6
SobreiraV <sobreirav@gmail.com>
2023-03-04 14:10:51 -0300

    blur-background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,63 @@
 const menuOpen = document.getElementById('hamburger_menu-open');
 
 menuOpen.addEventListener('click', () => {
+    const blurBackground = document.createElement('div');
     const asideMenu = document.createElement('aside');
     const menuButton = document.createElement('div');
     const menuLogo = document.createElement('img');
     const navMenu = document.createElement('nav');
     const ulList = document.createElement('ul');
+    const linkedinIcon = document.createElement('img');
+    const instagramIcon = document.createElement('img');
+    const youtubeIcon = document.createElement('img');
+    const linkedinLink = document.createElement('a');
+    const instagramLink = document.createElement('a');
+    const youtubeLink = document.createElement('a');
 
+    blurBackground.classList.add('blur-background');
     asideMenu.classList.add('aside-menu');
     menuLogo.classList.add('aside-menu-logo');
     menuButton.classList.add('material-symbols-outlined', 'hamburguer_menu-button', 'unselectable');
     menuButton.setAttribute('id', 'hamburger_menu-close');
+    linkedinIcon.classList.add('aside-social-icon');
+    instagramIcon.classList.add('aside-social-icon');
+    youtubeIcon.classList.add('aside-social-icon');
     
     menuButton.textContent = 'close';
 
+    document.body.appendChild(blurBackground);
     asideMenu.appendChild(menuButton);
     asideMenu.appendChild(menuLogo);
     asideMenu.appendChild(navMenu);
     navMenu.appendChild(ulList);
+    asideMenu.appendChild(linkedinLink);
+    asideMenu.appendChild(instagramLink);
+    asideMenu.appendChild(youtubeLink);
+    linkedinLink.appendChild(linkedinIcon);
+    instagramLink.appendChild(instagramIcon);
+    youtubeLink.appendChild(youtubeIcon);
 
     menuLogo.setAttribute('src', '/assets/img/logo-reprogramese/preferencial-horizontal.png');
     menuLogo.setAttribute('alt', 'Logo Reprograme-se');
+
+    linkedinIcon.setAttribute('src', '/assets/img/icon-social/linkedin.png');
+    instagramIcon.setAttribute('src', '/assets/img/icon-social/instagram.png');
+    youtubeIcon.setAttribute('src', '/assets/img/icon-social/youtube.png');
+
+    linkedinLink.setAttribute('href', '#');
+    instagramLink.setAttribute('href', '#');
+    youtubeLink.setAttribute('href', '#');
 
     const navLinks = [
         { text: 'Início', href: '#' },
         { text: 'O Programa', href: '#' },
         { text: 'Como funciona', href: '#' },
         { text: 'Inscrição', href: '#' },
-        { text: 'Contato', href: '#' }
+        { text: 'Notícias', href: '#' },
+        { text: 'Agenda', href: '#' },
+        { text: 'Dúvidas', href: '#' },
+        { text: 'Fale conosco', href: '#' },
+        { text: 'Parceiros', href: '#' }
     ];
 
     navLinks.forEach(navLink => {
@@ -43,5 +73,6 @@ menuOpen.addEventListener('click', () => {
 
     menuButton.addEventListener('click', () => {
         asideMenu.remove();
+        blurBackground.remove();
     });
 });
