b5d42d7005b465d304de8505e1e4362c6561c528
SobreiraV <sobreirav@gmail.com>
2023-03-05 16:53:19 -0300

    botao reset

--- assets/js/acessibilidade.js
+++ assets/js/acessibilidade.js
@@ -1,14 +1,24 @@
- // Função para aumentar e diminuir o tamanho da fonte
- const fontSizeIncreaseButton = document.getElementById('font-size-increase');
- const fontSizeDecreaseButton = document.getElementById('font-size-decrease');
+const fontSizeIncreaseButton = document.getElementById('font-size-increase');
+const fontSizeDecreaseButton = document.getElementById('font-size-decrease');
+const fontSizeResetButton = document.getElementById('font-size-reset');
 
- let fontSize = 16;
- fontSizeIncreaseButton.addEventListener('click', () => {
-   fontSize += 2;
-   document.body.style.fontSize = `${fontSize}px`;
- });
+let fontSize = localStorage.getItem('fontSize') || 16;
+document.body.style.fontSize = `${fontSize}px`;
 
- fontSizeDecreaseButton.addEventListener('click', () => {
-   fontSize -= 2;
-   document.body.style.fontSize = `${fontSize}px`;
- });
+fontSizeIncreaseButton.addEventListener('click', () => {
+  fontSize += 2;
+  document.body.style.fontSize = `${fontSize}px`;
+  localStorage.setItem('fontSize', fontSize);
+});
+
+fontSizeDecreaseButton.addEventListener('click', () => {
+  fontSize -= 2;
+  document.body.style.fontSize = `${fontSize}px`;
+  localStorage.setItem('fontSize', fontSize);
+});
+
+fontSizeResetButton.addEventListener('click', () => {
+  fontSize = 16;
+  document.body.style.fontSize = `${fontSize}px`;
+  localStorage.removeItem('fontSize');
+});
