cb5f6ee3659276ef3ed2a57b41f650613243fab6
SobreiraV <sobreirav@gmail.com>
2023-03-05 14:47:01 -0300

    font local storage

--- assets/js/font-changer.js
+++ assets/js/font-changer.js
@@ -1,14 +1,29 @@
 const fontChangerBtn = document.getElementById('font-changer');
 let fontToggle = false;
+let currentFont = localStorage.getItem('font');
+
+if (currentFont) {
+    applyFont(currentFont);
+}
 
 fontChangerBtn.addEventListener('click', function () {
+    if (fontToggle) {
+        applyFont('Open Sans');
+    } else {
+        applyFont('OpenDyslexic');
+    }
+    fontToggle = !fontToggle;
+});
+
+function applyFont(font) {
     const elements = document.querySelectorAll('*');
     for (let i = 0; i < elements.length; i++) {
-        if (fontToggle) {
+        if (font === 'Open Sans') {
             elements[i].classList.remove('alt-font');
         } else {
             elements[i].classList.add('alt-font');
         }
+        elements[i].style.fontFamily = font;
     }
-    fontToggle = !fontToggle;
-});
+    localStorage.setItem('font', font);
+}
